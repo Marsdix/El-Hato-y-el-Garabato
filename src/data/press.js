1687fffc2b578a4baba5ef048e949c789ed6752f
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 <guillejmp00@gmail.com>
2026-05-15 09:46:56 +0200

    Enlaces de periodico

--- src/data/press.js
+++ src/data/press.js
@@ -2,14 +2,44 @@
 // href → URL del artículo; dejar null hasta tener el enlace
 
 export const PRESS_LOGOS = [
-  { nombre: 'Robert Parker Wine Advocate',          href: null },
-  { nombre: 'James Suckling · Top 100 Value Wines', href: null },
-  { nombre: 'Condé Nast Traveler',                  href: null },
-  { nombre: 'Spanish Wine Lover',                   href: null },
-  { nombre: 'El País',                              href: null },
-  { nombre: 'La Vanguardia',                        href: null },
-  { nombre: 'Planeta Vino',                         href: null },
-  { nombre: 'Peñín',                                href: null },
-  { nombre: 'Sobremesa',                            href: null },
-  { nombre: 'ABC',                                  href: null },
-]
+  {
+    nombre: "Robert Parker Wine Advocate",
+    href: "https://elhatoyelgarabato.com/el-hato-y-el-garabato-en-robert-parker-wine-advocate/",
+  },
+  {
+    nombre: "James Suckling · Top 100 Value Wines",
+    href: "https://elhatoyelgarabato.com/eclectico-barrica-2023-en-el-top-100-value-wines-2025-de-james-suckling-con-94-puntos/",
+  },
+  {
+    nombre: "Condé Nast Traveler",
+    href: "https://elhatoyelgarabato.com/los-arribes-del-duero-entre-los-mejores-destinos-del-vino-del-mundo-para-2024-segun-conde-nast-traveler/",
+  },
+  {
+    nombre: "Spanish Wine Lover",
+    href: "https://elhatoyelgarabato.com/el-hato-y-el-garabato-en-spanish-wine-lover-el-perfil-completo-de-la-bodega/",
+  },
+  {
+    nombre: "El País",
+    href: "https://elhatoyelgarabato.com/el-hato-y-el-garabato-en-el-pais-los-vinos-de-sayago-que-llegan-a-japon/",
+  },
+  {
+    nombre: "La Vanguardia",
+    href: "https://elhatoyelgarabato.com/el-hato-y-el-garabato-en-la-vanguardia-el-vino-de-arribes-que-rompe-el-determinismo-latitudinal/",
+  },
+  {
+    nombre: "Planeta Vino",
+    href: "https://elhatoyelgarabato.com/el-hato-y-el-garabato-en-planeta-vino-la-esperanza-de-un-paisaje-en-los-arribes-del-duero/",
+  },
+  {
+    nombre: "!Hola¡",
+    href: "https://elhatoyelgarabato.com/el-hato-y-el-garabato-en-hola-parada-recomendada-en-la-ruta-por-los-arribes-del-duero/",
+  },
+  {
+    nombre: "Sobremesa",
+    href: "https://elhatoyelgarabato.com/el-hato-y-el-garabato-en-sobremesa-los-vinos-singulares-de-los-arribes-del-duero/",
+  },
+  {
+    nombre: "ABC",
+    href: "https://elhatoyelgarabato.com/de-buena-jera-en-abc-el-tinto-con-casi-100-anos-de-historia-familiar/",
+  },
+];
